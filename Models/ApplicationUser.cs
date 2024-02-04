e576d2eae1db85253f56b16e91a748f9b9a055b0
Elmer Alexis Muguerza Prieto <124643592+AlexMP-Dev@users.noreply.github.com>
2024-02-03 18:33:38 -0600

    Update ApplicationUser.cs

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,7 +1,7 @@
 using AspNetCore.Identity.MongoDbCore.Models;
 using MongoDbGenericRepository.Attributes;
 
-namespace pidepeapi.Models
+namespace baseApiNetCoreMongoDB-Security.Models
 {
     [CollectionName("users")]
     public class ApplicationUser : MongoIdentityUser<Guid>
